7330a9295be5d0da7124d86f35b1f393ed09b5af
ayushhchaudhary <ayushchaudhary3101@gmail.com>
2025-07-08 16:57:03 +0530

    hero section complete, featured section complete, navbar complete. used Clerk for authentication and user management. enabled multi user.

---
+++ src/pages/Favorite.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+const Favorite = () => {
+  return (
+    <>
+    </>
+  )
+}
+
+export default Favorite
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,14 @@
-import React from 'react'
+import React from "react";
+import HeroSection from "../components/HeroSection";
+import FeaturedSection from "../components/FeaturedSection";
 
 const Home = () => {
   return (
-    <div>Home</div>
-  )
-}
+    <>
+      <HeroSection />
+      <FeaturedSection />
+    </>
+  );
+};
 
-export default Home
+export default Home;
