2715383a04f335876c1a1b5d03de5d4b8c41ba0e
ayushhchaudhary <ayushchaudhary3101@gmail.com>
2025-07-22 04:40:58 +0530

    test commit

--- backend/inngest/index.js
+++ backend/inngest/index.js
@@ -1,5 +1,9 @@
 import { Inngest } from "inngest";
 
+//create a lient to send and recieve events
 export const inngest = new Inngest({ id: "movie-ticket-booking" });
 
+//inngest function to save user data to a database
+
+//create an empty array where we'll export future innget functions
 export const functions = [];
