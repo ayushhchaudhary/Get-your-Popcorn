bee82b561f7f73b6f4554e12b7545da7f8110d02
ayushhchaudhary <ayushchaudhary3101@gmail.com>
2025-07-22 04:20:37 +0530

    #. Restructure: moved all existing frontend code into 'client' folder

---
+++ backend/server.js
@@ -0,0 +1,23 @@
+import express from "express";
+import cors from "cors";
+import "dotenv/config";
+import connectDB from "./configs/db.js";
+import { clerkMiddleware } from "@clerk/express";
+import { Inngest } from "inngest";
+
+const app = express();
+const port = 3000;
+
+await connectDB();
+
+// Middleware
+app.use(express.json());
+app.use(cors());
+app.use(clerkMiddleware());
+
+//API routes
+app.get("/", (req, res) => res.send("Server is Live!"));
+
+app.listen(port, () =>
+  console.log(`Server Listening at http://localhost:${port}`)
+);
