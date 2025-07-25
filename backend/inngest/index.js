ef1377d8edb354b83638a5d3a5ad377422148fa2
ayushhchaudhary <ayushchaudhary3101@gmail.com>
2025-07-25 17:14:10 +0530

    #. User model import fix

--- backend/inngest/index.js
+++ backend/inngest/index.js
@@ -1,5 +1,5 @@
 import { Inngest } from "inngest";
-import User from "../models/User";
+import User from "../models/User.js";
 
 //create a lient to send and recieve events
 export const inngest = new Inngest({ id: "movie-ticket-booking" });
@@ -38,14 +38,14 @@ const syncUserUpdation = inngest.createFunction(
   async ({ event }) => {
     const { id, first_name, last_name, email_addresses, image_url } =
       event.data;
-      const userData = {
-        _id: id,
-        email: email_addresses[0].email_address,
-        name: first_name + " " + last_name,
-        image: image_url,
-      };
-      await User.findByIdAndUpdate(id,userData)
+    const userData = {
+      _id: id,
+      email: email_addresses[0].email_address,
+      name: first_name + " " + last_name,
+      image: image_url,
+    };
+    await User.findByIdAndUpdate(id, userData);
   }
 );
 
-export const functions = [syncUserCreation, syncUserDeletion,syncUserUpdation];
+export const functions = [syncUserCreation, syncUserDeletion, syncUserUpdation];
