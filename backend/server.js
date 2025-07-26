cfdd8f888ce23524261ed98c556da2fe8ae92c45
ayushhchaudhary <ayushchaudhary3101@gmail.com>
2025-07-26 16:40:11 +0530

    #. fixed bugs in server.js and index.js

--- backend/server.js
+++ backend/server.js
@@ -14,11 +14,7 @@ await connectDB();
 // Middleware
 app.use(express.json());
 app.use(cors());
-app.use(
-  clerkMiddleware({
-    apiKey: process.env.CLERK_SECRET_KEY,
-  })
-);
+app.use(clerkMiddleware());
 
 //API routes
 app.get("/", (req, res) => res.send("Server is Live!"));
@@ -27,4 +23,3 @@ app.use("/api/inngest", serve({ client: inngest, functions }));
 app.listen(port, () =>
   console.log(`Server Listening at http://localhost:${port}`)
 );
-
